9fe847dd9c907f35eddec51e726eba494bf50be5
marrymaxandroid <zeeshan.ahmad@chicsol.com>
2018-05-30 17:19:20 +0500

    my profile status updates

--- app/src/main/java/com/chicsol/marrymax/fragments/AccountSetting/MyProfileStatusFragment.java
+++ app/src/main/java/com/chicsol/marrymax/fragments/AccountSetting/MyProfileStatusFragment.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -41,10 +43,14 @@ public class MyProfileStatusFragment extends Fragment {
 
     String Tag = "MyProfileStatusFragment";
     private ProgressBar pDialog;
-    LinearLayout llCompleteProfile, llVeriyEmail, llVerifyPhone, llAdminReview;
-    TextView tvTitle, tvDesc;
+    LinearLayout llCompleteProfile, llVeriyEmail, llVerifyPhone, llAdminReview, llPhoneVerified;
+    RelativeLayout rlEmailVerified;
+    TextView tvDesc;
     private Context context;
+    TextView tvEmail, tvTitleLiveNotLive;
 
+
+    LinearLayout llASEmail, llASPhone;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -93,15 +99,42 @@ public class MyProfileStatusFragment extends Fragment {
         llVerifyPhone = (LinearLayout) view.findViewById(R.id.LinearLayoutMyProfileStatusVerifyPhone);
         llVeriyEmail = (LinearLayout) view.findViewById(R.id.LinearLayoutMyProfileStatusVerifyEmail);
         llAdminReview = (LinearLayout) view.findViewById(R.id.LinearLayoutMyProfileStatusAdminApproval);
+
+        llPhoneVerified = (LinearLayout) view.findViewById(R.id.LinearLayoutVerifyMobile);
+        rlEmailVerified = (RelativeLayout) view.findViewById(R.id.RelativeLayoutVerifyEmail);
+
+        tvEmail = (TextView) view.findViewById(R.id.TextViewMyProfileStatusEmail);
+
+        llASEmail = (LinearLayout) view.findViewById(R.id.LinearlayoutMyProfileStatusEmail);
+        llASPhone = (LinearLayout) view.findViewById(R.id.LinearlayoutMyProfileStatusPhone);
+
+
         String compUptoSSeventyText = "Dear <b> <font color=#216917>" + SharedPreferenceManager.getUserObject(context).getAlias() + "</font></b>, your profile is <b> <font color=#9a0606>Not Live </font></b> ";
+
+        tvTitleLiveNotLive = (TextView) view.findViewById(R.id.TextViewMyProfileStatusTitle);
+
+
+        tvEmail.setText(SharedPreferenceManager.getUserObject(getContext()).get_email());
+
+
+        if (SharedPreferenceManager.getUserObject(getContext()).get_member_status() < 3 || SharedPreferenceManager.getUserObject(getContext()).get_member_status() >= 7) {
+            checkEmailStatus(getContext());
+            // llASEmail.setVisibility(View.VISIBLE);
+        } else {
+            rlEmailVerified.setVisibility(View.VISIBLE);
+            llASEmail.setVisibility(View.GONE);
+        }
+
+
         getProfileCompletion();
+
     }
 
     private void setListeners() {
         llCompleteProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MarryMax marryMax=new MarryMax(getActivity());
+                MarryMax marryMax = new MarryMax(getActivity());
                 marryMax.getProfileProgress(getContext(), SharedPreferenceManager.getUserObject(getContext()), getActivity());
             }
         });
@@ -143,6 +176,17 @@ public class MyProfileStatusFragment extends Fragment {
                             Log.e("Email Complete Status", "" + dashboards.getEmail_complete_status());
 
                             //   tvProfileCompleteion.setText(dashboards.getProfile_complete_status() + "% Complete");
+
+
+                            if (Integer.parseInt(dashboards.getProfile_complete_status()) >= 70) {
+
+                                String compUptoSSeventyText = "Dear <b> <font color=#216917>" + SharedPreferenceManager.getUserObject(context).getAlias() + "</font></b>, your profile is <b> <font color=#9a0606> Live </font></b> ";
+                                tvTitleLiveNotLive.setText(Html.fromHtml(compUptoSSeventyText));
+
+                            } else {
+                                String compUptoSSeventyText = "Dear <b> <font color=#216917>" + SharedPreferenceManager.getUserObject(context).getAlias() + "</font></b>, your profile is <b> <font color=#9a0606>Not Live </font></b> ";
+                                tvTitleLiveNotLive.setText(Html.fromHtml(compUptoSSeventyText));
+                            }
 
 
                             if (dashboards.getProfile_complete_status().equals("100")) {
@@ -224,6 +268,102 @@ public class MyProfileStatusFragment extends Fragment {
                 VolleyLog.d("Err", "Error: " + error.getMessage());
                 //  pDialog.hide();
                 pDialog.setVisibility(View.INVISIBLE);
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return Constants.getHashMap();
+            }
+        };
+
+        req.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getInstance(context).addToRequestQueue(req, Tag);
+    }
+
+
+    public void checkEmailStatus(final Context context) {
+
+        // pDialog.show();
+        Log.e("status URL", Urls.getProfileCompletion + SharedPreferenceManager.getUserObject(context).get_path());
+        JsonArrayRequest req = new JsonArrayRequest(Urls.getProfileCompletion + SharedPreferenceManager.getUserObject(context).get_path(),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e("Response", response.toString());
+
+
+                        try {
+
+
+                            JSONArray jsonCountryStaeObj = response.getJSONArray(0);
+                            Gson gsonc;
+                            GsonBuilder gsonBuilderc = new GsonBuilder();
+                            gsonc = gsonBuilderc.create();
+                            Type listType = new TypeToken<Dashboards>() {
+                            }.getType();
+
+                            Dashboards dashboards = (Dashboards) gsonc.fromJson(jsonCountryStaeObj.getJSONObject(0).toString(), listType);
+
+
+                            boolean pCOmpleteStatus = true;
+                            Members members = SharedPreferenceManager.getUserObject(context);
+                            //   if (dashboards.getAdmin_approved_status().equals("1") && members.get_member_status() < 3) {
+
+                            if (dashboards.getEmail_complete_status().equals("1")) {
+                                //hide update email
+                              /*  etAsEmail.setKeyListener(null);
+                                etAsEmail.setEnabled(false);*/
+                                llASEmail.setVisibility(View.GONE);
+                                rlEmailVerified.setVisibility(View.VISIBLE);
+
+                            } else {
+                                //show
+                                rlEmailVerified.setVisibility(View.GONE);
+                                llASEmail.setVisibility(View.VISIBLE);
+
+                            }
+
+
+                            if (dashboards.getPhone_complete_status().equals("1")) {
+                                //hide update email
+                              /*  etAsEmail.setKeyListener(null);
+                                etAsEmail.setEnabled(false);*/
+                                llASPhone.setVisibility(View.GONE);
+                                llPhoneVerified.setVisibility(View.VISIBLE);
+
+                            } else {
+                                //show
+                                llASPhone.setVisibility(View.GONE);
+                                llPhoneVerified.setVisibility(View.VISIBLE);
+
+                            }
+
+                                /*else if (members.get_member_status() == 2) {
+                                    if (dashboards.getEmail_complete_status().equals("1")  && dashboards.getProfile_complete_status().equals("100")) {
+                                        members.set_member_status(3);
+                                        SharedPreferenceManager.setUserObject(context,members);
+                                    }
+
+                                }*/
+                            //  }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            //pDialog.hide();
+
+                        }
+
+                        //   pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Err", "Error: " + error.getMessage());
+                //  pDialog.hide();
 
             }
         }) {
