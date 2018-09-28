fc0bbfcbe8b974906b4305261f8a9ea4ebd65128
marrymaxandroid <zeeshan.ahmad@chicsol.com>
2018-09-28 17:27:18 +0500

    bug fixing 42

--- app/build/generated/source/r/debug/com/stripe/android/R.java
+++ app/build/generated/source/r/debug/com/stripe/android/R.java
@@ -543,20 +543,20 @@ public final class R {
         public static final int ic_error_amex = 0x7f0800be;
         public static final int ic_jcb = 0x7f0800c5;
         public static final int ic_mastercard = 0x7f0800ca;
-        public static final int ic_unknown = 0x7f0800e8;
-        public static final int ic_visa = 0x7f0800eb;
-        public static final int notification_action_background = 0x7f0800ff;
-        public static final int notification_bg = 0x7f080100;
-        public static final int notification_bg_low = 0x7f080101;
-        public static final int notification_bg_low_normal = 0x7f080102;
-        public static final int notification_bg_low_pressed = 0x7f080103;
-        public static final int notification_bg_normal = 0x7f080104;
-        public static final int notification_bg_normal_pressed = 0x7f080105;
-        public static final int notification_icon_background = 0x7f080106;
-        public static final int notification_template_icon_bg = 0x7f080107;
-        public static final int notification_template_icon_low_bg = 0x7f080108;
-        public static final int notification_tile_bg = 0x7f080109;
-        public static final int notify_panel_notification_icon_bg = 0x7f08010a;
+        public static final int ic_unknown = 0x7f0800e9;
+        public static final int ic_visa = 0x7f0800ec;
+        public static final int notification_action_background = 0x7f080100;
+        public static final int notification_bg = 0x7f080101;
+        public static final int notification_bg_low = 0x7f080102;
+        public static final int notification_bg_low_normal = 0x7f080103;
+        public static final int notification_bg_low_pressed = 0x7f080104;
+        public static final int notification_bg_normal = 0x7f080105;
+        public static final int notification_bg_normal_pressed = 0x7f080106;
+        public static final int notification_icon_background = 0x7f080107;
+        public static final int notification_template_icon_bg = 0x7f080108;
+        public static final int notification_template_icon_low_bg = 0x7f080109;
+        public static final int notification_tile_bg = 0x7f08010a;
+        public static final int notify_panel_notification_icon_bg = 0x7f08010b;
     }
     public static final class id {
         public static final int action0 = 0x7f09031e;
